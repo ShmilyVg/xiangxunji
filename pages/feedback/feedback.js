bf7ee6bc021814a984c3de47110df7d5dd3741bd
liubiao <liubiao@vlifepaper.com>
2019-11-08 16:52:10 +0800

    添加意见反馈

---
+++ pages/feedback/feedback.js
@@ -0,0 +1,28 @@
+// import Protocol from "../../apis/network/protocol";
+
+Page({
+    data: {},
+    async savePic() {
+        // const {statusCode, tempFilePath} = await Protocol.downloadFile({url: 'https://backend.stage.hipee.cn/hipee-web-hiecg/img/mine_fankui_hipeeerweima.png'});
+        // if (statusCode === 200) {
+        //     wx.saveImageToPhotosAlbum({
+        //         filePath: tempFilePath,
+        //         success(res) {
+        //             wx.showToast({
+        //                 title: '保存成功',
+        //                 icon: 'success',
+        //                 duration: 2000
+        //             })
+        //         },
+        //         fail(res) {
+        //             console.log(res)
+        //             wx.showToast({
+        //                 title: '保存失败',
+        //                 icon: 'fail',
+        //                 duration: 2000
+        //             })
+        //         }
+        //     })
+        // }
+    }
+})
--- pages/user-center/user-center.js
+++ pages/user-center/user-center.js
@@ -1,23 +1,35 @@
 // pages/user-center/user-center.js
+import {ConnectState} from "../../modules/bluetooth/bluetooth-state";
 import HiNavigator from "../../navigator/hi-navigator";
 
+const app = getApp();
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
+        connectState: ''
     },
-    addDeviceEvent() {
-        HiNavigator.navigateToConnectDevice();
+
+    reconnectEvent() {
+        const connectState = app.getBLEManager().getBLELatestConnectState();
+        if (connectState === ConnectState.CONNECTING || connectState === ConnectState.CONNECTED) {
+            return;
+        }
+        app.getBLEManager().connect();
     },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-
+        this.setData({
+            connectState: app.getBLEManager().getBLELatestConnectState()
+        });
     },
-
+    toFeedbackPage() {
+        HiNavigator.navigateToFeedbackPage();
+    },
     /**
      * 生命周期函数--监听页面显示
      */

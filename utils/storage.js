ce6723efa1e3028feb939d7b6ec69f0696af9229
liubiao <liubiao@vlifepaper.com>
2019-11-19 17:33:12 +0800

    头像布局问题

--- utils/storage.js
+++ utils/storage.js
@@ -59,7 +59,8 @@ export default class Storage extends BaseStorage {
     }
 
     static getIndexPageRemindHaveShow() {
-        return this.getSync('index_page_remind_have_show');
+        return false
+        // return this.getSync('index_page_remind_have_show');
     }
 }
 

54fe19cf99b9591fb505ea9a58ee4f1c40fa35e2
liubiao <liubiao@vlifepaper.com>
2019-12-02 17:25:20 +0800

    使用https

--- utils/config.js
+++ utils/config.js
@@ -2,9 +2,9 @@ import {NetworkConfig} from "../modules/network/network/index";
 
 const Release = false,
     SoftwareVersion = `${Release ? '' : '香薰机_Stage '}v0.1.0`,
-    PostUrl = `http://backend.${Release ? '' : 'stage.'}hipee.cn/hipee-web-aroma/`,
+    PostUrl = `https://backend.${Release ? '' : 'stage.'}hipee.cn/hipee-web-aroma/`,
     UploadUrl = 'https://backend.hipee.cn/hipee-upload/hibox/mp/upload/image.do',
-    VoiceUrl = 'http://img.hipee.cn/aroma/',
+    VoiceUrl = 'https://img.hipee.cn/aroma/',
     DIALOG_BG_ANIMATION_DURATION = 400;
 
 NetworkConfig.setConfig({postUrl: PostUrl});

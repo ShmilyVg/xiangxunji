4cdfdeb1b9ed38f8ff8c852b0f5f1116aa1107dc
liubiao <liubiao@vlifepaper.com>
2019-12-03 16:01:55 +0800

    保存客服图片

--- pages/feedback/feedback.js
+++ pages/feedback/feedback.js
@@ -4,22 +4,20 @@ import {Toast} from "heheda-common-view";
 Page({
     data: {},
     async savePic() {
-        const {statusCode, tempFilePath} = await Protocol.downloadFile({url: 'https://backend.stage.hipee.cn/hipee-web-hiecg/img/mine_fankui_hipeeerweima.png'});
-        if (statusCode === 200) {
-            wx.saveImageToPhotosAlbum({
-                filePath: tempFilePath,
-                success: (res) => {
-                    Toast.success('保存成功');
-                },
-                fail: (res) => {
-                    console.log(res)
-                    wx.showToast({
-                        title: '保存失败',
-                        icon: 'fail',
-                        duration: 2000
-                    })
-                }
-            })
-        }
+        const {tempFilePath} = await Protocol.downloadFile({url: 'https://backend.stage.hipee.cn/hipee-web-hiecg/img/mine_fankui_hipeeerweima.png'});
+        wx.saveImageToPhotosAlbum({
+            filePath: tempFilePath,
+            success: (res) => {
+                Toast.success('保存成功');
+            },
+            fail: (res) => {
+                console.log(res)
+                wx.showToast({
+                    title: '保存失败',
+                    icon: 'fail',
+                    duration: 2000
+                })
+            }
+        })
     }
 })
